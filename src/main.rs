e947fdd448463c485e6d9cb3fd458d2b39b20f00
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2023-02-21 18:15:38 +0200

    Refactor analysis in loop

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,127 @@
 use compression::{audio, fft, plotting};
 use inquire::Select;
+use num_complex::Complex32;
 
 const OUTPUT_DIR: &str = "data/";
 
-enum Round {
-    Up,
-    Down,
+fn main() {
+    load_data();
 }
 
-struct AnalysisConfig {
-    pub name: String,
-    pub rounding: Round,
-    pub output_dir: String,
-    pub print_progress: bool,
+fn load_data() {
+    loop {
+        let options = vec!["Custom", "File", "Exit"];
+        let mode = Select::new("Select Data:", options)
+            .prompt_skippable()
+            .unwrap();
+        let (mut waveform, sample_rate, name) = match mode {
+            Some("File") => {
+                if let Some(file) = prompt_file() {
+                    get_wav_file(file)
+                } else {
+                    break;
+                }
+            }
+            Some("Custom") => get_custom(),
+            _ => break,
+        };
+        analyze_waveform(&mut waveform, sample_rate, &name);
+    }
 }
 
-impl AnalysisConfig {
-    fn new(name: &str, rounding: Round, output_dir: &str, print_progress: bool) -> AnalysisConfig {
-        AnalysisConfig {
-            name: name.to_owned(),
-            rounding,
-            output_dir: output_dir.to_owned(),
-            print_progress,
+fn get_wav_file(file: String) -> (Vec<f32>, usize, String) {
+    let (header, waveform) = compression::audio::load_wav_file(&file).unwrap();
+    println!("{:?}", header);
+    (waveform, header.sampling_rate as usize, file)
+}
+
+fn get_custom() -> (Vec<f32>, usize, String) {
+    let waveform = [0., 22937., 32767., 22937., 0., -22937., -32767., -22937.].to_vec();
+    let sample_rate = 44100;
+    (waveform, sample_rate, String::from("custom waveform"))
+}
+
+fn analyze_waveform(waveform: &mut Vec<f32>, sample_rate: usize, name: &str) {
+    prompt_round_sample(waveform);
+    let sample_size = waveform.len();
+    let time_domain = fft::convert_sample(&waveform);
+    loop {
+        let option = match prompt_analysis_option() {
+            Some(opt) => opt,
+            None => break,
+        };
+        match option {
+            AnalysisOption::Plot => {
+                plot_time_domain(waveform.clone(), sample_rate as u32, name);
+                let freq_domain = fft::fft(&time_domain);
+                let freq_resolution = sample_rate as f32 / sample_size as f32;
+                plot_freqbins(&freq_domain, freq_resolution, name);
+            }
+            AnalysisOption::Export => {
+                let modified_waveform = time_domain.iter().map(|x| x.re as i16).collect();
+                export_waveform(modified_waveform, sample_rate as u32);
+            }
         }
     }
 }
 
-fn prompt_file() -> String {
+enum AnalysisOption {
+    Plot,
+    Export,
+}
+
+fn prompt_analysis_option() -> Option<AnalysisOption> {
+    let option_names: Vec<String> = [
+        "Plot domains".to_string(),
+        "Export waveform".to_string(),
+        "Back".to_string(),
+    ]
+    .to_vec();
+    let uinput = Select::new("Options:", option_names)
+        .prompt_skippable()
+        .unwrap();
+    if let Some(option) = uinput {
+        match option.as_str() {
+            "Plot domains" => Some(AnalysisOption::Plot),
+            "Export waveform" => Some(AnalysisOption::Export),
+            _ => None,
+        }
+    } else {
+        None
+    }
+}
+
+fn plot_time_domain(time_domain: Vec<f32>, sample_rate: u32, name: &str) {
+    plotting::plot(
+        time_domain,
+        1. / sample_rate as f32,
+        &format!("{} - time domain", &name),
+        &format!("{}time.html", OUTPUT_DIR),
+        "Time (seconds)",
+        "Amplitude",
+    );
+}
+
+fn plot_freqbins(freq_domain: &Vec<Complex32>, freq_resolution: f32, name: &str) {
+    println!("Drawing frequencies...");
+    let freq_bins = fft::frequency_bins(freq_domain);
+    plotting::plot(
+        freq_bins,
+        freq_resolution,
+        &format!("{} - frequency domain", name),
+        &format!("{}freq.html", OUTPUT_DIR),
+        "Frequency (Hz)",
+        "Amplitude",
+    );
+}
+
+fn export_waveform(waveform: Vec<i16>, sample_rate: u32) {
+    let path = format!("{}waveform.wav", OUTPUT_DIR);
+    println!("Exporting {path}");
+    audio::write_wav_file(&path, waveform, sample_rate).expect("failed to write file");
+}
+
+fn prompt_file() -> Option<String> {
     let mut files = Vec::new();
     for path in std::fs::read_dir(OUTPUT_DIR).unwrap() {
         let p = path
@@ -40,97 +135,17 @@ fn prompt_file() -> String {
             files.push(p);
         }
     }
-    Select::new("File:", files).prompt().unwrap()
+    Select::new("File:", files).prompt_skippable().unwrap()
 }
 
-fn prompt_rounding() -> Round {
+fn prompt_round_sample(vec: &mut Vec<f32>) {
     let selection = Select::new("Round to 2^n:", vec!["up", "down"])
-        .prompt()
+        .prompt_skippable()
         .unwrap();
-    if selection == "up" {
-        return Round::Up;
+    match selection {
+        Some("up") => fft::round_sample_size_up(vec),
+        Some("down") => fft::round_sample_size_down(vec),
+        _ => (),
     }
-    Round::Down
-}
-
-fn get_wav_file() -> (Vec<f32>, usize, String) {
-    let file = prompt_file();
-    let (header, waveform) = compression::audio::load_wav_file(&file).unwrap();
-    println!("Sample size: {}\n{:?}", waveform.len(), header);
-    (waveform, header.sampling_rate as usize, file)
-}
-
-fn get_custom() -> (Vec<f32>, usize, String) {
-    let waveform = [0., 22937., 32767., 22937., 0., -22937., -32767., -22937.].to_vec();
-    let sample_rate = 44100;
-    (waveform, sample_rate, String::from("custom waveform"))
-}
-
-fn analyze_waveform(waveform: Vec<f32>, sample_rate: usize, config: AnalysisConfig) {
-    if config.print_progress {
-        println!("Converting...");
-    }
-    let mut waveform = waveform;
-    match config.rounding {
-        Round::Up => fft::round_sample_size_up(&mut waveform),
-        Round::Down => fft::round_sample_size_down(&mut waveform),
-    }
-    let time_domain = fft::convert_sample(&waveform);
-    if config.print_progress {
-        println!("Transforming...");
-    }
-    let freq_domain = fft::fft(&time_domain);
-    if config.print_progress {
-        println!("Analysing...");
-    }
-    let freq_bins = fft::frequency_bins(&freq_domain);
-    if config.print_progress {
-        println!("Drawing waveform...");
-    }
-    let sample_size = waveform.len();
-    plotting::plot(
-        waveform,
-        1. / sample_rate as f32,
-        &format!("{} - time domain", config.name),
-        &format!("{}time.html", config.output_dir),
-        "Time (seconds)",
-        "Amplitude",
-    );
-    if config.print_progress {
-        println!("Drawing frequencies...");
-    }
-    let freq_resolution = sample_rate as f32 / sample_size as f32;
-    plotting::plot(
-        freq_bins,
-        freq_resolution,
-        &format!("{} - frequency domain", config.name),
-        &format!("{}freq.html", config.output_dir),
-        "Frequency (Hz)",
-        "Amplitude",
-    );
-    if config.print_progress {
-        println!("Writing wav to file...");
-    }
-    let waveform32 = time_domain.iter().map(|x| x.re as i16).collect();
-    audio::write_wav_file(
-        &format!("{}waveform.wav", config.output_dir),
-        waveform32,
-        sample_rate as u32,
-    )
-    .expect("failed to write file");
-    if config.print_progress {
-        println!("Analysis written to disk.");
-    }
-}
-
-fn main() {
-    let options = vec!["Custom", "File"];
-    let mode = Select::new("Select Mode:", options).prompt().unwrap();
-    let (waveform, sample_rate, name) = match mode {
-        "File" => get_wav_file(),
-        _ => get_custom(),
-    };
-    let rounding = prompt_rounding();
-    let config = AnalysisConfig::new(&name, rounding, OUTPUT_DIR, true);
-    analyze_waveform(waveform, sample_rate, config);
+    println!("New sample size: {}", vec.len());
 }
